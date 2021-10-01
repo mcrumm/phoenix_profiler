470e87c87567757dd99f471d19e8bd8bd0780fd8
Michael Crumm <michael.crumm@cargosense.com>
2021-09-30 21:00:47 -0700

    loosen lv dependency

--- mix.exs
+++ mix.exs
@@ -24,8 +24,7 @@ defmodule FriendsOfPhoenix.Debug.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.6.0"},
-      {:phoenix_live_view, "~> 0.16",
-       github: "phoenixframework/phoenix_live_view", optional: true},
+      {:phoenix_live_view, "~> 0.14"},
       {:jason, "~> 1.0", only: [:dev, :test, :docs]},
       {:ex_doc, "~> 0.25", only: :docs}
     ]
