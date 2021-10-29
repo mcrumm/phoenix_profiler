4db197531cdbdefe2bd00cfcddae2b8c8862f062
Michael Crumm <michael.crumm@cargosense.com>
2021-10-28 17:05:31 -0700

    Update deps

--- mix.exs
+++ mix.exs
@@ -38,8 +38,9 @@ defmodule PhoenixProfiler.MixProject do
 
   defp deps do
     [
-      {:phoenix, "~> 1.5"},
-      {:phoenix_live_view, "~> 0.14"},
+      {:phoenix, "~> 1.4.7 or ~> 1.5.0 or ~> 1.6.0"},
+      {:phoenix_live_view, "~> 0.14.3 or ~> 0.15.0 or ~> 0.16.0 or ~> 0.17.0"},
+      {:phoenix_live_dashboard, "~> 0.3.0 or ~> 0.4.0 or ~> 0.5.0 or ~> 0.6.0"},
       {:phoenix_live_reload, "~> 1.3", only: :dev},
       {:plug_cowboy, "~> 2.0", only: :dev},
       {:jason, "~> 1.0", only: [:dev, :test, :docs]},
