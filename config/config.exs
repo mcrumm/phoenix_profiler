ce9a0969844da7b863c37f8c23e203e58caa2c77
Michael Crumm <mike@crumm.net>
2021-10-31 15:20:11 -0700

    Loosen Elixir version requirement (#31)
    * Loosen Elixir version requirement
    
    * Do not yet upgrade to latest mime
    
    * Move esbuild config into dev script
    
    * Update to latest esbuild

--- config/config.exs
+++ config/config.exs
@@ -6,12 +6,3 @@ config :logger, level: :warn
 config :logger, :console, format: "[$level] $message\n"
 
 config :phoenix, :stacktrace_depth, 20
-
-# Configure esbuild (the version is required)
-config :esbuild,
-  version: "0.12.21",
-  default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../static/assets),
-    cd: Path.expand("../dev/assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-  ]
