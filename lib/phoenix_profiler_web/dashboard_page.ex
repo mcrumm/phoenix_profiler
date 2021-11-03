1c1e1176d50ac65d614bc9b311c94d237a42229a
Michael Crumm <michael.crumm@cargosense.com>
2021-11-02 17:15:21 -0700

    [WIP] Endpoint pages

--- lib/phoenix_profiler_web/dashboard_page.ex
+++ lib/phoenix_profiler_web/dashboard_page.ex
@@ -38,34 +38,123 @@ if Code.ensure_loaded?(Phoenix.LiveDashboard) do
     end
 
     @impl true
-    def render_page(%{profile: profile}) do
+    def render_page(assigns) do
+      name = inspect(DemoWeb.Endpoint)
+
       nav_bar(
         items: [
-          request: [
-            name: "Request",
-            render: fn ->
-              {PhoenixWeb.Profiler.DashboardPage.RequestComponent, %{profile: profile}}
-            end
-          ]
-        ]
+          {name,
+           name: name,
+           method: :redirect,
+           render: fn ->
+             if assigns[:profile] do
+               conn = assigns.profile.conn
+
+               nav_bar(
+                 items: [
+                   path_params: [
+                     name: "Path Params",
+                     render: fn -> render_params_table(conn, :path_params) end
+                   ],
+                   query_params: [
+                     name: "Query Params",
+                     render: fn -> render_params_table(conn, :query_params) end
+                   ],
+                   body_params: [
+                     name: "Body Params",
+                     render: fn -> render_params_table(conn, :body_params) end
+                   ],
+                   request_headers: [
+                     name: "Request Headers",
+                     render: fn -> render_params_table(conn, :req_headers) end
+                   ],
+                   request_cookies: [
+                     name: "Request Cookies",
+                     render: fn -> render_params_table(conn, :req_cookies) end
+                   ],
+                   response_headers: [
+                     name: "Response Headers",
+                     render: fn -> render_params_table(conn, :resp_headers) end
+                   ],
+                   response_cookies: [
+                     name: "Response Cookies",
+                     render: fn -> render_params_table(conn, :resp_cookies) end
+                   ],
+                   private: [
+                     name: "Private",
+                     render: fn -> render_params_table(conn, :private) end
+                   ],
+                   request: [
+                     name: "Request",
+                     render: fn ->
+                       {PhoenixWeb.Profiler.DashboardPage.RequestComponent,
+                        %{profile: assigns.profile}}
+                     end
+                   ]
+                 ],
+                 extra_params: [:endpoint, :token]
+               )
+             else
+               table(
+                 columns: columns(),
+                 id: :phxweb_profiler_profilers_table,
+                 row_attrs: &row_attrs/1,
+                 row_fetcher: &fetch_profiles/2,
+                 rows_name: "profiles",
+                 title: "Phoenix Profiler"
+               )
+             end
+           end}
+        ],
+        nav_param: :endpoint,
+        extra_params: [:nav],
+        style: :bar
       )
     end
 
-    def render_page(_assigns) do
+    defp render_params_table(conn, field, title \\ nil) do
       table(
-        columns: columns(),
-        id: :phxweb_profiler_profilers_table,
-        row_attrs: &row_attrs/1,
-        row_fetcher: &fetch_profiles/2,
-        rows_name: "profiles",
-        title: "Phoenix Profiler"
+        id: :"#{field}_table",
+        columns: [
+          %{
+            field: :key,
+            sortable: :asc
+          },
+          %{
+            field: :value,
+            sortable: nil
+          }
+        ],
+        row_fetcher: fn _params, _node ->
+          rows =
+            case Map.get(conn, field) do
+              %Plug.Conn.Unfetched{} ->
+                []
+
+              params when is_map(params) or is_list(params) ->
+                for {key, value} <- params,
+                    do: %{key: pp(key), value: pp(value)}
+            end
+
+          {rows, length(rows)}
+        end,
+        title: title || Phoenix.Naming.humanize(field)
       )
     end
+
+    # for printing
+    defp pp(val) when is_binary(val) or is_number(val), do: val
+    defp pp(val), do: inspect(val)
 
     @impl true
     def handle_event("show_profile", %{"token" => token}, socket) do
-      token_path = live_dashboard_path(socket, socket.assigns.page, %{token: token})
-      {:noreply, push_patch(socket, to: token_path)}
+      token_path =
+        live_dashboard_path(socket, socket.assigns.page, %{
+          endpoint: inspect(DemoWeb.Endpoint),
+          token: token
+        })
+
+      {:noreply, push_redirect(socket, to: token_path)}
     end
 
     defp fetch_profiles(params, node) do
